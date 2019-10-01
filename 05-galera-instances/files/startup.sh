89115d785a1ebc69780830be2638287b4592e5fd
Eric Malloy <em@galt.dev>
2019-10-01 00:50:57 -0400

    add stackdriver monitoring agent
    offload config of monitoring agent to a phase two function to be done after galera bootstrap

--- 05-galera-instances/files/startup.sh
+++ 05-galera-instances/files/startup.sh
@@ -36,11 +36,28 @@ if [[ ! $(dpkg --list | grep google-fluent) ]]; then
         https://dl.google.com/cloudagents/install-logging-agent.sh \
         && bash /tmp/install-logging-agent.sh
 else
-  echo "stackdriver logging agent already installed! Continue!"
+  echo "stackdriver logging agent already installed! Continue ..."
 fi
 }
 
-function galera_repo {
+
+function monitoring_agent() {
+   #https://cloud.google.com/monitoring/agent/
+   if [[ ! $(dpkg --list | grep stackdriver-agent ) ]]; then
+     echo "Installing stackdriver monitoring agent ..."
+     curl -sS -o /tmp/install-monitoring-agent.sh \
+      https://dl.google.com/cloudagents/install-monitoring-agent.sh \
+      && bash /tmp/install-monitoring-agent.sh
+   else
+     echo "stackdriver monitoring agent already installed! Continue ..."
+   fi
+}
+
+
+function galera_repo() {
+export DEBIAN_FRONTEND=noninteractive
+apt-get update \
+&& apt-get install dirmngr -qy
 
 if [[ ! -f /etc/apt/sources.list.d/galera.list ]]; then
 cat << EOF > /etc/apt/sources.list.d/galera.list
@@ -84,12 +101,51 @@ EOF
 fi
 }
 
+
+function 03_config_stackdriver_agent() {
+
+cat << EOF > /tmp/0303_config_stackdriver_agent.sh_
+
+#https://cloud.google.com/monitoring/agent/plugins/mysql
+# https://collectd.org/wiki/index.php/Plugin:MySQL
+
+local STATS_USER="stackdriver_user"
+local STATS_PASS="thisIsADemoanditIsInsecure"
+cat << EOF > /tmp/collectd.sql
+CREATE USER ${STATS_USER}@'%' IDENTIFIED BY "${STATS_PASS}";
+GRANT USAGE ON *.* TO ${STATS_USER}@'localhost';
+GRANT REPLICATION CLIENT ON *.* TO ${STATS_USER}@'localhost';
+EOF
+cat /tmp/collectd.sql | mysql -uroot
+
+cat << EOF > /opt/stackdriver/collectd/etc/collectd.d/mysql.conf
+LoadPlugin mysql
+<Plugin "mysql">
+    # Each database needs a separate Database section.
+    # Replace DATABASE_NAME in the Database section with the name of the database.
+    <Database "DATABASE_NAME">
+        # When using non-standard MySQL configurations, replace the below with
+        #Host "MYSQL_HOST"
+        #Port "MYSQL_PORT"
+        Host "localhost"
+        Port 3306
+        User "${STATS_USER}"
+        Password "${STATS_PASS}"
+        MasterStats true
+        SlaveStats true
+    </Database>
+</Plugin>
+EOF
+touch /tmp/.mysql-stackdriver-been-setup
+service stackdriver-agent restart
+EOF
+}
+
 check_disk
 logging_agent
+monitoring_agent
 galera_repo
 galera_conf
-if [[ ! -f /tmp/.mysql-stackdriver-been-setup ]]; then
-  configure_mysql_monitoring_agent
-fi
 
+03_config_stackdriver_agent
 
